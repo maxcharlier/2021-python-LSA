2d0e264d9710a5b4d7e3735dda9621f4eb7dff94
Maximilien Charlier <neo-sw@hotmail.com>
2021-10-27 12:01:15 +0200

    upadte distribution figures

--- python/example/bunch/var_distrib/distribution.py
+++ python/example/bunch/var_distrib/distribution.py
@@ -20,6 +20,7 @@ import csv
 import math
 from scipy.stats import truncnorm
 import matplotlib.patches as mpatches
+import numpy as np
 
 
 
@@ -67,18 +68,34 @@ def distribution_graph(input_csv_file, output_file="tag_per_cells.pdf", title="T
   #plot
   fig, ax = plt.subplots()
   max_x = max([len(x) for x in bar_values])
+
+  #add 2 colors in the color cycle
+  colors = list(plt.cm.tab10(np.arange(10))) + ["crimson", "indigo"]
+  ax.set_prop_cycle('color', colors)
+
   # print(bar_values)
   for i in range(len(bar_values)):
       bar_plot = ax.bar([(x*2)+(i*(1.0/len(bar_values))) for x in range(0, len(bar_values[i]))],[(x/sum(bar_values[i]))*100 for x in bar_values[i]])
+      # Creating legend with color box
       if i == 0:
         color = bar_plot.patches[-1].get_facecolor()
         # Creating legend with color box
-        legend_handles.append(mpatches.Patch(color=color, label="Uniform distribution"))
+        legend_handles.append(mpatches.Patch(color=color, label="Uniform distribution"))  
+        legend_handles.append(mpatches.Patch(color='black', label="Mean values"))    
+      if i == 1:
+        color = bar_plot.patches[-1].get_facecolor()
+        # Creating legend with color box
+        legend_handles.append(mpatches.Patch(color=color, label="Run 1"))
+        legend_handles.append(mpatches.Patch(color="white", label="   ..."))    
+      if i == len(bar_values)-1:
+        color = bar_plot.patches[-1].get_facecolor()
+        # Creating legend with color box
+        legend_handles.append(mpatches.Patch(color=color, label="Run "+str(len(bar_values)-1)))
 
   plt.xticks([(x*2)+0.5 for x in range(0, max_x)], range(0, max_x)) 
   plt.yticks([i*10 for i in range(0, 11)]) 
   plt.xlabel('Number of tags in a cells')
-  plt.ylabel('Probabibilty [\%]')
+  plt.ylabel('Fraction of cells [\%]')
   plt.grid(color='tab:grey', linestyle='--', linewidth=1, alpha=0.3)
   plt.title(title)
   plt.legend(handles=legend_handles)
@@ -92,6 +109,11 @@ def distribution_graph(input_csv_file, output_file="tag_per_cells.pdf", title="T
   #plot2
   fig, ax = plt.subplots()
 
+
+  #add 2 colors in the color cycle
+  colors = list(plt.cm.tab10(np.arange(10))) + ["crimson", "indigo"]
+  ax.set_prop_cycle('color', colors)
+
   ax_secondary = ax.twiny()
   max_x = max([len(x) for x in bar_values])
 
@@ -101,86 +123,48 @@ def distribution_graph(input_csv_file, output_file="tag_per_cells.pdf", title="T
   # print([(refresh[0]*R[0])/i for i in range(1, max_x)])
   # print([(x*2)+0.5 for x in range(1, max_x)])
   for i in range(0, len(bar_values)):
+    nb_tags = sum([((bar_values[i][y]*y)) for y in range(1, len(bar_values[i]))])
     if i == 0:
-      bar_plot = ax.bar([i_to_x(x) for x in range(1, len(bar_values[i]))],[(x/(sum(bar_values[i])-bar_values[i][0]))*100 for x in bar_values[i][1:]])
+      bar_plot = ax.bar([i_to_x(x) for x in range(1, len(bar_values[i]))],[((bar_values[i][y]*y)/nb_tags*100) for y in range(1, len(bar_values[i]))])
     else:
-      bar_plot = ax.bar([i_to_x(x)-0.5+(i*(1.0/len(bar_values))) for x in range(1, len(bar_values[i]))],[(x/(sum(bar_values[i])-bar_values[i][0]))*100 for x in bar_values[i][1:]])
-  #theoretical curve   
-  # theorical_value = []
-  # myclip_a = 0
-  # my_mean = R[0] #number of tag per cells
-  # myclip_b=8
-  # if R[0] == 2 :
-  #   my_std=1.25
-  # else:
-  #   my_std=1.55
-  # a, b = (myclip_a - my_mean) / my_std, (myclip_b - my_mean) / my_std
-  # # print((a,b))
-
-  # # plt.plot([(x*2)+0.5 for x in range(1, max_x)], [x*100 for x in truncnorm.pdf(range(1, max_x), a, b, loc=2.0)])  
-  # # plt.plot([(x*2)+0.5 for x in range(1, max_x)], [x*100 for x in truncnorm.pdf(range(-1, max_x-2), -1, 6, loc=0)])  
-  # theorical_pdf = truncnorm.pdf(range(0, max_x), a, b, loc=my_mean, scale=my_std)
-  # theorical_pdf[1] = theorical_pdf[0] + theorical_pdf[1]
-  # for i in range(1, max_x):
-  #   # print(float(theorical_pdf[i]))
-  #   theorical_value.append(theorical_pdf[i]*100)
-  # plt.plot([(x*2)+0.5 for x in range(1, max_x)], theorical_value, color='black')
-  # # Creating legend with color box
-  # legend_handles.append(mpatches.Patch(color='black', label="Theoretical cfd"))  
+      bar_plot = ax.bar([i_to_x(x)-0.5+(i*(1.0/len(bar_values))) for x in range(1, len(bar_values[i]))],[((bar_values[i][y]*y)/nb_tags*100) for y in range(1, len(bar_values[i]))])
+  
 
   bar_mean= [0 for x in range(1, max([len(bar_values[i]) for i in range(1, len(bar_values))]))]
   # print("bar mean " + str(bar_mean))
   for i in range(1, len(bar_values)):
     j=0
+    nb_tags = sum([((bar_values[i][y]*y)) for y in range(1, len(bar_values[i]))])
     # print(bar_values[i][1:])
-    for x in bar_values[i][1:]:
-      bar_mean[j]+= (x/(sum(bar_values[i])-bar_values[i][0]))*100 
+    for y in range(1, len(bar_values[i])):
+      bar_mean[j]+= ((bar_values[i][y]*y)/nb_tags)*100 
       j+=1
 
   for i in range(0, len(bar_mean)):
     bar_mean[i] = bar_mean[i]/(len(bar_values)-1)
+  print("sum mean " + str(sum(bar_mean)))
 
   plt.plot([i_to_x(i) for i in range(1, max_x)], bar_mean, color='black')
 
   # print("sum mean " +str(sum(bar_mean)))
-  # Creating legend with color box
-  legend_handles.append(mpatches.Patch(color='black', label="Mean values"))
-
-  # def pgcd(a,b):
-  #   if a == b:
-  #     return a
-  #   #pcgd need that a >= b
-  #   if a < b:
-  #     (a,b) = (b,a)
-  #   while a!=b: 
-  #     d=abs(b-a) 
-  #     b=a 
-  #     a=d 
-  #   return d
-  # plt.xticks([(x*2)+0.5 for x in range(1, max_x)], [str(i)+"\n"+str(round((refresh[0]*math.floor(R[0]/i)),2)) if R[0]>= i else str(i)+"\n"+str(round((refresh[0]/math.ceil(i/R[0])),2)) for i in range(1, max_x)])
-
-  # if update_rate_s :
-  #   plt.xticks([(x*2)+0.5 for x in range(1, max_x)], [str(i)+"\n"+str(round((update_rate[0]*math.ceil(i/R[0])),2)) for i in range(1, max_x)]) 
-  # else:
-  #   plt.xticks([(x*2)+0.5 for x in range(1, max_x)], [str(i)+"\n"+str(round((refresh[0]/math.ceil(i/R[0])),2)) for i in range(1, max_x)]) 
 
 
   #axis ticks and label
   #bottom x axis
   ax.set_xticks([i_to_x(i) for i in range(1, max_x)]) 
   ax.set_xticklabels([str(i) for i in range(1, max_x)]) 
-  ax.set_xlabel('Tags per cells')
+  ax.set_xlabel('Number of tags in a cells')
 
   #top x axis
   ax_secondary.set_xlim(ax.get_xlim())
   # print(ax.get_xlim())
   ax_secondary.set_xticks([i_to_x(i) for i in range(1, max_x)])
   # print(ax.get_xticks())
-  ax_secondary.set_xticklabels([(str(round((refresh[0]/(x/2)/R[0]),3)) if x > 0 else '') for x in ax_secondary.get_xticks()])
+  ax_secondary.set_xticklabels([(str(round((refresh[0]/(x/2)*R[0]),3)) if x > 0 else '') for x in ax_secondary.get_xticks()])
   ax_secondary.set_xlabel('Localisation update for each tag [Hz]')
   #yaxis
   plt.yticks([i*10 for i in range(0, 11)]) 
-  ax.set_ylabel('Probabibilty [\%]')
+  ax.set_ylabel('Fraction of tags [\%]')
   plt.ylim((0,100))
 
   ax.grid(color='tab:grey', linestyle='--', linewidth=1, alpha=0.3)
